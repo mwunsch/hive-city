c61e838be17d0004f55f23337767e90ce097baa4
Mark Wunsch <mark@markwunsch.com>
2016-08-31 12:49:35 -0400

    run Main through elm format

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,12 @@
-import Model exposing (Model)
+module Main exposing (..)
 
+import Model exposing (Model)
 import Html exposing (Html)
 import Html.App as App
 import Html.Events exposing (onClick)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+
 
 main : Program Never
 main =
@@ -15,59 +17,83 @@ main =
         , subscriptions = subscriptions
         }
 
+
+
 -- MODEL
 
+
 type alias GameState =
-    { fighter: Model
+    { fighter : Model
     , playerSelection : Maybe Model
     }
 
-init : (GameState, Cmd Msg)
+
+init : ( GameState, Cmd Msg )
 init =
-    ({ fighter = Model.averageFighter { x = 50
-                                      , y = 50
-                                      , z = 0
-                                      }
-     , playerSelection = Nothing
-     }
-    , Cmd.none)
+    ( { fighter =
+            Model.averageFighter
+                { x = 50
+                , y = 50
+                , z = 0
+                }
+      , playerSelection = Nothing
+      }
+    , Cmd.none
+    )
+
+
 
 -- UPDATE
 
-type Msg =
-    Select Model
 
-update : Msg -> GameState -> (GameState, Cmd Msg)
+type Msg
+    = Select Model
+
+
+update : Msg -> GameState -> ( GameState, Cmd Msg )
 update msg game =
     case msg of
         Select fighter ->
-            ({ game | playerSelection = Just fighter }, Cmd.none)
+            ( { game | playerSelection = Just fighter }, Cmd.none )
+
+
 
 -- SUBSCRIPTIONS
+
 
 subscriptions : GameState -> Sub Msg
 subscriptions game =
     Sub.none
 
+
+
 -- VIEW
+
 
 view : GameState -> Html Msg
 view game =
     let
-        fighter = "@"
+        fighter =
+            "@"
 
         color =
             case game.playerSelection of
-                Nothing -> "black"
-                Just x -> "red"
+                Nothing ->
+                    "black"
+
+                Just x ->
+                    "red"
     in
         svg [ viewBox "0 0 100 100", width "100%" ]
-            [ text' [ fontSize "20"
-                    , fontFamily "monospace"
-                    , textAnchor "middle"
-                    , onClick (Select game.fighter)
-                    , Svg.Attributes.cursor "pointer"
-                    , x (toString game.fighter.position.x)
-                    , y (toString game.fighter.position.y)
-                    , fill color ] [text fighter]
+            [ text'
+                [ fontSize "20"
+                , fontFamily "monospace"
+                , textAnchor "middle"
+                , onClick (Select game.fighter)
+                , Svg.Attributes.cursor "pointer"
+                , x (toString game.fighter.position.x)
+                , y (toString game.fighter.position.y)
+                , fill color
+                ]
+                [ text fighter ]
             ]
