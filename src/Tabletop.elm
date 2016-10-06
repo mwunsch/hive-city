552e81926d5ee17d90dd23765d3dd971b05b0577
Mark Wunsch <mark@markwunsch.com>
2016-10-06 16:26:00 -0400

    Find distance between two points on the tabletop

--- src/Tabletop.elm
+++ src/Tabletop.elm
@@ -36,15 +36,31 @@ positionFromMouseCoords ( x, y ) scale =
         transform a =
             round <| (toFloat a) / scale
 
-        x' = transform x
+        x' =
+            transform x
 
-        y' = transform y
+        y' =
+            transform y
     in
         ( x', y' )
 
 
 type alias Inch =
     Int
+
+
+distance : Position -> Position -> Float
+distance ( x1, y1 ) ( x2, y2 ) =
+    let
+        y' =
+            (y1 - y2) ^ 2
+
+        x' =
+            (x1 - x2) ^ 2
+    in
+        (x' + y')
+            |> toFloat
+            |> sqrt
 
 
 view : Tabletop -> List (Svg msg) -> Svg msg
