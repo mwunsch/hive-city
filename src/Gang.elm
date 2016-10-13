3d597be450ea7e10abe9a81db789e23471d98663
Mark Wunsch <mark@markwunsch.com>
2016-10-13 11:38:11 -0400

    Generate a Gang!

--- src/Gang.elm
+++ src/Gang.elm
@@ -1,10 +1,31 @@
 module Gang exposing (..)
 
 import Dict exposing (Dict)
+import List
 import Model exposing (Model, Id)
+import Random exposing (Generator)
 import Svg exposing (Svg)
 
-type alias Gang = Dict Id Model
+
+type alias Gang =
+    Dict Id Model
+
+
+empty : Gang
+empty =
+    Dict.empty
+
+
+fromList : List Model -> Gang
+fromList =
+    Dict.fromList << List.map (\m -> ( m.id, m ))
+
+
+generator : Generator Gang
+generator =
+    Random.list 5 Model.generator
+        |> Random.map fromList
+
 
 view : Gang -> (Model -> msg) -> List (Svg msg)
 view gang msg =
