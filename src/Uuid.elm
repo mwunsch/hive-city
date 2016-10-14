3d70b375879aab3edb5cc3355f16a408c5320d57
Mark Wunsch <mark@markwunsch.com>
2016-10-14 10:48:42 -0400

    Make a dumb uuid generator

---
+++ src/Uuid.elm
@@ -0,0 +1,36 @@
+module Uuid exposing (Uuid, uuid)
+
+import Random exposing (Generator, andThen)
+import String
+import Array exposing (Array)
+import Maybe exposing (withDefault)
+
+
+type alias Uuid =
+    String
+
+
+uuid : Generator Uuid
+uuid =
+    let
+        hexgen =
+            Random.int 0 15
+
+        listgen : Generator (List a)
+        listgen =
+            Random.map (always []) Random.bool
+
+        numToHex : Int -> Char
+        numToHex =
+            (flip Array.get) hexTable >> withDefault 'x'
+    in
+        [ 8, 4, 4, 4, 12 ]
+            |> List.map (\num -> Random.list num hexgen)
+            |> List.foldr (Random.map2 (::)) listgen
+            |> Random.map (List.map (List.map numToHex >> String.fromList))
+            |> Random.map (String.join "-")
+
+
+hexTable : Array Char
+hexTable =
+    Array.fromList [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' ]
