4215c5c94565efb245da08eb2a7ce9c5d2b3042c
Mark Wunsch <mark@markwunsch.com>
2016-11-07 16:25:30 -0500

    Begin writing out types for weapons

--- src/Weapons.elm
+++ src/Weapons.elm
@@ -1,10 +1,51 @@
 module Weapons exposing (..)
 
+import Tabletop exposing (Inch)
+
 
 type Weapon
-    = Combat
-    | Pistol
-    | Basic
-    | Special
-    | Heavy
-    | Grenade
+    = Combat Profile
+    | Pistol Profile
+    | Basic Profile
+    | Special Profile
+    | Heavy Profile
+    | Grenade Profile
+
+
+type alias Profile =
+    { range : Range
+    , toHit : ( Maybe Modifier, Maybe Modifier )
+    , strength : Int
+    , damage : Int
+    , saveModifier : Maybe Modifier
+    , ammoRoll : Maybe Int
+    }
+
+
+type Range
+    = Close
+    | Ranged Short Long
+
+
+type alias Short =
+    ( Inch, Inch )
+
+
+type alias Long =
+    ( Inch, Inch )
+
+
+type alias Modifier =
+    Int -> Int
+
+
+knife : Weapon
+knife =
+    Combat
+        { range = Close
+        , toHit = ( Nothing, Nothing )
+        , strength = 1
+        , damage = 1
+        , saveModifier = Nothing
+        , ammoRoll = Nothing
+        }
