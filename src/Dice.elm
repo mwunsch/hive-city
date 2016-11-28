13a6a06dd938468ed10e136a73a0b5137fe8dad5
Mark Wunsch <mark@markwunsch.com>
2016-11-28 16:03:34 -0500

    Add Dice module

---
+++ src/Dice.elm
@@ -0,0 +1,38 @@
+module Dice exposing (..)
+
+import Random exposing (Generator)
+import List
+
+
+type alias Dice =
+    Generator (List Int)
+
+
+{-| The `d` function is a constructor function for dice. Like
+`andThen`, the arguments are reversed. If the rulebook says "2D6", you
+would use the pipe argument like:
+
+    2 |> d 6
+
+-}
+d : Int -> Int -> Dice
+d val num =
+    Random.int 1 val
+        |> Random.list num
+
+
+d6 : Int -> Dice
+d6 num =
+    num |> d 6
+
+
+{-| From the rulebook:
+
+> There isn’t actually any such thing as a 3- sided dice, instead a D3
+> means roll a D6 and halve the result rounding up.
+
+-}
+d3 : Int -> Dice
+d3 num =
+    d6 num
+        |> Random.map (List.map (toFloat >> (*) 0.5 >> ceiling))
