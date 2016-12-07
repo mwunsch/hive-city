e9b86e1efb0dbf52c7b3aa735d190e6a080f1be6
Mark Wunsch <mark@markwunsch.com>
2016-12-07 11:38:02 -0500

    Game turn is pair of turn, player

--- src/Game.elm
+++ src/Game.elm
@@ -2,12 +2,31 @@ module Game exposing (..)
 
 import Player exposing (Player)
 import Tabletop exposing (Tabletop)
+import Turn exposing (Turn)
 
 
 type alias Game =
     { players : ( Player, Player )
     , tabletop : Tabletop
+    , turn : ( Turn, Player )
     }
+
+
+init : Game
+init =
+    let
+        table =
+            Tabletop.by 6 4
+
+        players =
+            ( Player.init table
+            , Player.init table
+            )
+    in
+        { players = players
+        , tabletop = table
+        , turn = ( Turn.init, Tuple.first players )
+        }
 
 
 player1 : Game -> Player
